a646e244e400792c3c704896131fb3daeca6fddf
yamaimo <yarnaimo@gmail.com>
2018-07-28 20:00:13 +0900

    Use firestore-simple and Implement Fires class

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,8 +1,20 @@
+import { CollectionReference, Query } from '@google-cloud/firestore'
 import admin from 'firebase-admin'
+import { FirestoreSimple } from 'firestore-simple'
 const key = require('../../config/service-account-key.json')
 
 admin.initializeApp({
     credential: admin.credential.cert(key),
 })
 
-export const db = admin.firestore()
+export const firestore = admin.firestore()
+
+export const getCollection = (path: string) => {
+    return new Fires(firestore, path)
+}
+
+export class Fires extends FirestoreSimple {
+    withQuery(fn: (ref: CollectionReference) => Query) {
+        return this.fetchByQuery(fn(this.collectionRef))
+    }
+}
