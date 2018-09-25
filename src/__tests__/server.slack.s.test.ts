5548b323483da14d01e61c1efc115a9ba0c6dd2b
yamaimo <yarnaimo@gmail.com>
2018-09-25 16:54:02 +0900

    Update tests

---
+++ src/__tests__/server.slack.s.test.ts
@@ -0,0 +1,46 @@
+import { cmd } from '.'
+import { Parts } from '../models/Schedule'
+
+let docId: string
+
+test('add schedule', async () => {
+    const parts = 'Hiru.1230+.1500.1530+Yoru.1730..1830'
+
+    const [schedule] = await cmd(
+        `s new -c live -t __test_1__ -d 000801 -p ${parts} -u https://google.com`
+    )
+
+    expect(schedule).toMatchObject({
+        category: 'live',
+        date: new Date(2000, 7, 1, 0, 0),
+        parts: Parts.parse(parts),
+    })
+    docId = schedule.id
+})
+
+test('update schedule', async () => {
+    const [schedule] = await cmd(`s update ${docId} -t "__test_1_modified__"`)
+
+    expect(schedule.title).toBe('__test_1_modified__')
+    expect(schedule.id).toBe(docId)
+})
+
+test('list schedules', async () => {
+    const [doc2] = await cmd(
+        's new -c live -t __test_2__ -d 000801 -u https://google.com'
+    )
+
+    const schedules = await cmd(
+        `s ls --since 000801 --until 000801 -t __test_1_m`
+    )
+
+    await cmd(`s delete ${doc2.id}`)
+
+    expect(schedules).toHaveLength(1)
+    expect(schedules[0].id).toBe(docId)
+})
+
+test('delete schedule', async () => {
+    const [schedule] = await cmd(`s delete ${docId}`)
+    expect(schedule.id).toBe(docId)
+})
---
+++ src/__tests__/tasks.SearchBatch.test.ts
@@ -0,0 +1,31 @@
+import { waitAll } from '@yarnaimo/arraymo'
+import { Status } from 'twitter-d'
+import { SearchState } from '../models/SearchState'
+import { SearchBatch } from '../tasks/SearchBatch'
+import { day } from '../utils/day'
+
+const batch = new SearchBatch()
+let _tweets: Status[] = []
+
+test('get tweets to classify', async () => {
+    const until = batch.now.subtract(10, 'minute')
+
+    _tweets = await batch.searchTweets('0', until)
+
+    const state = new SearchState('main', {
+        prevTweetId: _tweets[5].id_str,
+    })
+    await state.save()
+
+    expect(day(_tweets[0].created_at).isBefore(until)).toBeTruthy()
+})
+
+test('run', async () => {
+    const { tweetsToClassify, docs } = await batch.run()
+    expect(tweetsToClassify).toHaveLength(5)
+
+    const state = await SearchState.get('main')
+    expect(state!.prevTweetId).toBe(_tweets[0].id_str)
+
+    await waitAll(docs, d => d.delete())
+})
