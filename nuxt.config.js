8c97d005a68683213dde03b0ea2319d52193587d
yamaimo <yarnaimo@gmail.com>
2018-11-09 00:29:40 +0900

    Add pwa configs

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,5 @@
 const { extend } = require('./webpack-extend')
+const path = require('path')
 
 const meta = (name, content, options = {}) => ({
     ...(name.startsWith('og:') ? { property: name } : { name }),
@@ -6,34 +7,82 @@ const meta = (name, content, options = {}) => ({
     ...options,
 })
 
-const description = '上田麗奈さん非公式Info'
+const title = 'ReinaInfo'
+const shortDescription = '上田麗奈さん非公式Info'
+const description =
+    '声優・上田麗奈さんに関する情報を共有する非公式Webサイトです。'
+const screenName = '@Unoffishama'
+
+const iconSrc = path.resolve('web/static/icon.svg')
 
 module.exports = {
     mode: 'spa',
+    icon: false,
+    manifest: {
+        name: title,
+        short_name: title,
+        description: shortDescription,
+        lang: 'ja',
+        icons: [
+            {
+                src: '/icons/android-chrome-192x192.png',
+                sizes: '192x192',
+                type: 'image/png',
+            },
+            {
+                src: '/icons/android-chrome-512x512.png',
+                sizes: '512x512',
+                type: 'image/png',
+            },
+        ],
+    },
     head: {
-        titleTemplate: '%s | ReinaInfo',
+        titleTemplate: `%s | ${title}`,
         meta: [
             { charset: 'utf-8' },
             meta('viewport', 'width=device-width, initial-scale=1'),
             meta('description', description, { hid: 'description' }),
             // meta('og:url', 'https://domain.com' + this.$route.path),
             // meta('og:title', ''),
-            meta('og:description', description),
+            meta('og:description', shortDescription),
             // meta('og:image', ''),
             meta('og:type', 'website'),
             meta('twitter:card', 'summary'),
-            meta('twitter:site', '@Unoffishama'),
-            meta('twitter:creator', '@Unoffishama'),
+            meta('twitter:site', screenName),
+            meta('twitter:creator', screenName),
         ],
-        link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
-        script: [{ src: 'https://cdn.polyfill.io/v2/polyfill.min.js' }],
+        link: [
+            // ...icons,
+            {
+                rel: 'stylesheet',
+                href:
+                    'https://fonts.googleapis.com/css?family=Ubuntu:400,700|Julius+Sans+One',
+            },
+        ],
+        // script: [{ src: 'https://cdn.polyfill.io/v2/polyfill.min.js' }],
     },
     loading: { color: '#A18C7E' },
+
+    router: {
+        extendRoutes(routes, resolve) {
+            routes.push({
+                path: '/',
+                redirect: '/topic',
+            })
+        },
+    },
 
     generate: { dir: 'public' },
     srcDir: 'web',
 
-    modules: ['nuxt-typescript', 'nuxt-babel'],
+    css: ['ress', '@mdi/font/css/materialdesignicons.min.css'],
+    modules: [
+        ['nuxt-rfg-icon', { masterPicture: iconSrc }],
+        // '@nuxtjs/manifest',
+        '@nuxtjs/pwa',
+        'nuxt-typescript',
+        'nuxt-babel',
+    ],
     typescript: { tsconfig: './tsconfig.web.json' },
     plugins: [
         '~/plugins/global-css',
--- webpack-extend.js
+++ webpack-extend.js
@@ -1,9 +1,17 @@
 const alias = require('pring/webpack-alias')
 const tsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
+const path = require('path')
 
 const extend = config => {
-    if (!config.resolve.alias) config.resolve.alias = {}
-    config.resolve.alias = { ...config.resolve.alias, ...alias }
+    config.resolve.alias = {
+        ...(config.resolve.alias || {}),
+        ...alias,
+        'google-libphonenumber': path.resolve(
+            __dirname,
+            'google-libphonenumber'
+        ),
+        'firebase/auth': path.resolve(__dirname, 'google-libphonenumber'),
+    }
 
     config.resolve.plugins = [new tsconfigPathsPlugin()]
 }
